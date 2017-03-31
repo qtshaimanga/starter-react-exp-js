22de536ad2f087b6945b1e538e1de8e88fc1190d
quentin tshaimanga <q.tshaimanga@gmail.com>
2017-03-31 15:24:26 +0200

    test react css transition groupe

--- client/app/desktop/components/Menu/index.js
+++ client/app/desktop/components/Menu/index.js
@@ -1,16 +1,27 @@
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import { Link } from 'react-router-dom'
-
+import './Menu.styl'
 
 class Menu extends React.Component {
 
   render() {
 
     return(
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/whatever">Not Found</Link>
-      </ul>
+      <div>
+        <ReactCSSTransitionGroup
+        transitionName="pageSlider"
+        transitionAppear={true}
+        transitionAppearTimeout={5000}
+        transitionEnter={false}
+        transitionLeave={false}
+        className="page">
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/whatever">Not Found</Link>
+          </ul>
+        </ReactCSSTransitionGroup>
+      </div>
     )
 
   }
